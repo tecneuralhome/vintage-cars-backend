0b400d07879d657e327fcba4b673824bdf47eb48
Aashik <aashikbsc@gmail.com>
2025-01-24 15:38:38 -0800

    updated changes related dist

--- dist/vintage-cars-frontend/server/assets-chunks/index_csr_html.mjs
+++ dist/vintage-cars-frontend/server/assets-chunks/index_csr_html.mjs
@@ -3,7 +3,7 @@ export default `<!doctype html>
 <head>
   <meta charset="utf-8">
   <title>VintageCarsFrontend</title>
-  <base href="/">
+  <base href="./">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
   <!-- ===== GOOGLE FONTS ====== -->
--- dist/vintage-cars-frontend/server/assets-chunks/login_index_html.mjs
+++ dist/vintage-cars-frontend/server/assets-chunks/login_index_html.mjs
@@ -1,7 +1,7 @@
 export default `<!DOCTYPE html><html lang="en" data-beasties-container><head>
   <meta charset="utf-8">
   <title>VintageCarsFrontend</title>
-  <base href="/">
+  <base href="./">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
   <!-- ===== GOOGLE FONTS ====== -->
