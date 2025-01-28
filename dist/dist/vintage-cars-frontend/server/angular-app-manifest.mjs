f042f43286326a1c7976999ef12144315d8a9bf3
Aashik <aashikbsc@gmail.com>
2025-01-27 21:04:30 -0800

    updated changes

--- dist/dist/vintage-cars-frontend/server/angular-app-manifest.mjs
+++ dist/dist/vintage-cars-frontend/server/angular-app-manifest.mjs
@@ -23,18 +23,28 @@ export default {
   },
   {
     "renderMode": 2,
-    "route": "/notifications"
+    "route": "/booking-managements"
+  },
+  {
+    "renderMode": 2,
+    "route": "/user-list"
+  },
+  {
+    "renderMode": 2,
+    "route": "/cars-list"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 25159, hash: '69585e45dc09538bbe6bf43a8b7771975e019160687d383e246dfa047383b023', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 21275, hash: 'b16139519dfad0e9b13ab72edbb0dfe4a9bb8d2889a2bfb695f96b70b01fe06c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'add-cars/index.html': {size: 60600, hash: '003bd57e77e1e45d0acb2902c3a45ce24495b3882b68c128af30a00dbbe018ad', text: () => import('./assets-chunks/add-cars_index_html.mjs').then(m => m.default)},
-    'notifications/index.html': {size: 56930, hash: '8ee916cb9ce216f5630ff507bedb74c41ea877835f2cdfe8643c25aca2f4f2d6', text: () => import('./assets-chunks/notifications_index_html.mjs').then(m => m.default)},
-    'Home-page/index.html': {size: 92778, hash: '855562b0843c78cf7418701be592cbb170238b6fc5a723728eda8aba6c8c1961', text: () => import('./assets-chunks/Home-page_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 92778, hash: '855562b0843c78cf7418701be592cbb170238b6fc5a723728eda8aba6c8c1961', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'detail-page/index.html': {size: 66923, hash: '9417fe1c1c4ffa7104429f36b6a578d9fb2f7310e9b826cad427881ca9eb451e', text: () => import('./assets-chunks/detail-page_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 25159, hash: '47bbacdd6e33a5e825e4b4660af9b4920be6f50e5ac5e4b4d369667dbd1e278b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 21275, hash: 'd42ef86902fd915a6b933aa7f91990d115fbfa06921cfd10ea31a3c4fbbd80b8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'add-cars/index.html': {size: 60600, hash: '375a3b553fe50577135668ef742162d854e103c5dc84682cb0217ab19e4edd29', text: () => import('./assets-chunks/add-cars_index_html.mjs').then(m => m.default)},
+    'booking-managements/index.html': {size: 54816, hash: 'fbe4cfd66a8df4705e1ae08d8e1d9c74052ebe57e1d150f868552f518fbed3e5', text: () => import('./assets-chunks/booking-managements_index_html.mjs').then(m => m.default)},
+    'user-list/index.html': {size: 54847, hash: 'e5c7aaeaa8495ce6bd67555626071786faa7087998518dd715ae2dd865997da4', text: () => import('./assets-chunks/user-list_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 93258, hash: '5bd9db86485867bf1f3b09534abeae9b57b36405221952561c1a0c1929ede5a9', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'Home-page/index.html': {size: 93258, hash: '5bd9db86485867bf1f3b09534abeae9b57b36405221952561c1a0c1929ede5a9', text: () => import('./assets-chunks/Home-page_index_html.mjs').then(m => m.default)},
+    'detail-page/index.html': {size: 67435, hash: '7dd00ffb515b1850d853d2eadba7a68e82a7cd82b88670849f0b2f3751560bc8', text: () => import('./assets-chunks/detail-page_index_html.mjs').then(m => m.default)},
+    'cars-list/index.html': {size: 61081, hash: 'b3b2e33d2f2d5278ba8dedc8e65f028c4eaa15d06d66ecd57a2e2ab5af3b6fe2', text: () => import('./assets-chunks/cars-list_index_html.mjs').then(m => m.default)},
     'styles-DZ6UBGXD.css': {size: 231612, hash: 'B2Fy9V+bfZo', text: () => import('./assets-chunks/styles-DZ6UBGXD_css.mjs').then(m => m.default)}
   },
 };
