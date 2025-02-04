670464be4c10826efc52b8321148de985a5ab00d
Aashik <aashikbsc@gmail.com>
2025-02-03 18:46:31 -0800

    updated moved to the server

--- dist/dist/vintage-cars-frontend/server/assets-chunks/index_server_html.mjs
+++ dist/dist/vintage-cars-frontend/server/assets-chunks/index_server_html.mjs
@@ -2,10 +2,10 @@ export default `<!doctype html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>VintageCarsFrontend</title>
+  <title>Keyn Classics</title>
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="icon" type="image/x-icon" href="logo.png">
   <!-- ===== GOOGLE FONTS ====== -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
@@ -23,6 +23,6 @@ export default `<!doctype html>
 <body><script type="text/javascript" id="ng-event-dispatch-contract">(()=>{function p(t,n,r,o,e,i,f,m){return{eventType:t,event:n,targetElement:r,eic:o,timeStamp:e,eia:i,eirp:f,eiack:m}}function u(t){let n=[],r=e=>{n.push(e)};return{c:t,q:n,et:[],etc:[],d:r,h:e=>{r(p(e.type,e,e.target,t,Date.now()))}}}function s(t,n,r){for(let o=0;o<n.length;o++){let e=n[o];(r?t.etc:t.et).push(e),t.c.addEventListener(e,t.h,r)}}function c(t,n,r,o,e=window){let i=u(t);e._ejsas||(e._ejsas={}),e._ejsas[n]=i,s(i,r),s(i,o,!0)}window.__jsaction_bootstrap=c;})();
 </script>
   <app-root></app-root>
-<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="scripts-EEEIPNC3.js" defer=""></script><script src="main-JGAFV6AV.js" type="module"></script></body>
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="scripts-EEEIPNC3.js" defer=""></script><script src="main-2HORBHHV.js" type="module"></script></body>
 </html>
 `;
